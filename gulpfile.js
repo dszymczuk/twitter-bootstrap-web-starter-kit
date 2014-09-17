c01532b7097a689d666d127a06c9a4d1026fdff5
Damian Szymczuk <damian.szymczuk@comarch.pl>
2014-09-17 14:30:52 +0200

    change some path to bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,7 @@ gulp.task('styles', function () {
   return gulp.src([
       'app/styles/*.scss',
       'app/styles/**/*.css',
-      'app/styles/components/components.scss'
+      'app/styles/bootstrap.scss'
     ])
     .pipe($.changed('styles', {extension: '.scss'}))
     .pipe($.rubySass({
@@ -130,7 +130,7 @@ gulp.task('html', function () {
     .pipe(assets.restore())
     .pipe($.useref())
     // Update Production Style Guide Paths
-    .pipe($.replace('components/components.css', 'components/main.min.css'))
+    .pipe($.replace('bootstrap.css', 'bootstrap.min.css'))
     // Minify Any HTML
     .pipe($.if('*.html', $.minifyHtml()))
     // Output Files
